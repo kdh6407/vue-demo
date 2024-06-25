a05e0af1d9efb74b5d16e7ef0af75a9de4ccff38
kdh6407 <140301175+kdh6407@users.noreply.github.com>
2024-06-25 18:27:41 +0900

    Update App.vue

--- todo-demo/src/App.vue
+++ todo-demo/src/App.vue
@@ -13,7 +13,7 @@ import TodoTemplate from './components/TodoTemplate.vue';
 
 // 반응형 상태 선언
 const todos = reactive([
-  { id: 1, text: '리액트의 기초 알아보기', checked: true },
+  { id: 1, text: '뷰의 기초 알아보기', checked: true },
   { id: 2, text: '컴포넌트 스타일링해 보기', checked: true },
   { id: 3, text: '일정 관리 앱 만들어 보기', checked: false },
 ]);
